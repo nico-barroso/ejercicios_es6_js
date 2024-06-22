c00986ca0c6bc68192c9a6e582ed67ea61f378a3
Nico Barroso <josenicolasbarrosogarcia@gmail.com>
2024-06-22 22:37:43 +0100

    Ejercicio 1 commit.

---
+++ ejercicio1.js
@@ -0,0 +1,42 @@
+// 1.1 En base al siguiente javascript, crea variables en base a las propiedades
+// del objeto usando object destructuring e imprimelas por consola. Cuidado,
+// no hace falta hacer destructuring del array, solo del objeto.
+
+const game = {
+  title: "The last us 2",
+  gender: ["action", "zombie", "survival"],
+  year: 2020,
+};
+const { title, gender, year } = game;
+console.log(title, gender, year);
+
+// 1.2 En base al siguiente javascript, usa destructuring para crear 3 variables
+// llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+// imprimelo por consola.
+
+const fruits = ["Banana", "Strawberry", "Orange"];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1, fruit2, fruit3);
+
+// 1.3 En base al siguiente javascript, usa destructuring para crear 2
+// variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+  return { name: "Bengal Tiger", race: "Tiger" };
+};
+
+const { name: animalName, race } = animalFunction();
+
+console.log(animalName, race);
+
+// 1.4 En base al siguiente javascript, usa destructuring para crear las
+// variables name y itv con sus respectivos valores. Posteriormente crea
+// 3 variables usando igualmente el destructuring para cada uno de los años
+// y comprueba que todo esta bien imprimiendolo.
+
+const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
+const { name: carName, itv } = car;
+const [year1, year2, year3] = itv;
+
+console.log(carName, itv);
+console.log(year1, year2, year3);
